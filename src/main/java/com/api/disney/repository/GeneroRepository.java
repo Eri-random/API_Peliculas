e2d68cba833fb5f3d5f4b11a7ef2f6bffab0b02d
Eri-random <brenherrera@alumno.unlam.edu.ar>
2022-12-25 18:32:16 -0300

    Agregando Genero Repository

---
+++ src/main/java/com/api/disney/repository/GeneroRepository.java
@@ -0,0 +1,10 @@
+package com.api.disney.repository;
+
+import com.api.disney.model.Genero;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GeneroRepository extends JpaRepository<Genero,Long> {
+
+}
